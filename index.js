90450843a349667b320b69845f9cbf4a1ac5ac96
dipikaash <ash.dipika2012@gmail.com>
2023-08-10 15:25:03 +0530

    added time validation

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ console.log(data.Doctors);
 // submit event handler
 document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
-     const data = Object.fromEntries(new FormData(e.target).entries());
-  console.log(data);
+     const formData = Object.fromEntries(new FormData(e.target).entries());
+  console.log(formData);
+  //console.log(formData.aptmtTime.value <  data.Doctors[0].availability[0].startTime)
+  console.log("booked");
 });
-
 //dynamically added patients
 for (let patient of data.patientList) {
     var patientOpt = document.createElement("option");        
@@ -32,6 +33,21 @@ document.getElementById('patient').onchange = function (){
         if(doc.options[i].id === docId)
          doc.options[i].selected = true; 
     }
-
 }
 document.getElementById('patient').onchange();
+
+document.getElementById('aptmtTime').min = data.Doctors[0].availability[0].startTime;
+document.getElementById('aptmtTime').max = data.Doctors[0].availability[0].endTime;
+
+// document.getElementById('doc').onchange = function (){
+// const selDocId = this.id;
+// for(let i=0;i<data.Doctors.length;i++){
+//     if(data.Doctors[i].id === selDocId){
+//         document.getElementById('aptmtTime').min = data.Doctors[i].availability[0].startTime;
+//         document.getElementById('aptmtTime').max = data.Doctors[i].availability[0].endTime;
+//         console.log(document.getElementById('aptmtTime'));
+//     }
+//  }
+// }
+// document.getElementById('doc').onchange();
+
